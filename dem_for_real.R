7083e0976bb2ed8bdaa6226329c32b47c0540719
Erin Reilly <erinmariereilly@gmail.com>
2020-01-02 17:29:39 -0500

    was able to join the 4 DEM images, clip to the shapefile and extract the values for stoney.  Then unlisted and filtered for values less than 0.75 (MHHW)

--- dem_for_real.R
+++ dem_for_real.R
@@ -1,5 +1,6 @@
 library(stars)
 library(dplyr)
+#library(ggplot2)
 # You need the not CRAN version of stars because the st_mosaic feature doesn't work
 #in the CRAN version as of Dec 12, 2019
 
@@ -8,10 +9,10 @@ library(dplyr)
 #Tidal Station NAVD88 is equal to MSL so -Elevation can be considered Depth
 
 #List of Files to Upload
-q = c("F://USGS CoNED Topobathy/Chesapeake_Topobathy_DEM_v1_137.TIF",
-      "F://USGS CoNED Topobathy/Chesapeake_Topobathy_DEM_v1_138.TIF",
-       "F://USGS CoNED Topobathy/Chesapeake_Topobathy_DEM_v1_149.TIF",
-       "F://USGS CoNED Topobathy/Chesapeake_Topobathy_DEM_v1_150.TIF")
+q = c("M://USGS CoNED Topobathy/Chesapeake_Topobathy_DEM_v1_137.TIF",
+      "M://USGS CoNED Topobathy/Chesapeake_Topobathy_DEM_v1_138.TIF",
+       "M://USGS CoNED Topobathy/Chesapeake_Topobathy_DEM_v1_149.TIF",
+       "M://USGS CoNED Topobathy/Chesapeake_Topobathy_DEM_v1_150.TIF")
 
 #st_mosaic is essentially the same as "Mosaic to Raster" in ArcGIS
 #using the proxy=TRUE command means it loads the Metadata for the files
@@ -24,9 +25,55 @@ plot(dem)
 
 #load the shapefile from the geodatabase (.gdb)
 
-clip<-st_read("F://patapsco_fm.gdb", layer="patapsco")
+clip<-st_read("M://patapsco_fm.gdb", layer="patapsco")
+
+area<-clip %>% 
+  group_by(crk) %>% 
+  st_area()
 
 #clip the raster by the shapefile
 dem_clip<-dem[clip]
 plot(dem_clip)
 
+
+
+#separating the different creeks because I am not sure how I want to do this
+
+dem_bear<-dem[filter(clip, crk=="Bear")]
+dem_curtis<-dem[filter(clip, crk=="Curtis")]
+dem_ih<-dem[filter(clip, crk=="Inner Harbour")]
+dem_mb<-dem[filter(clip, crk=="Middle Branch")]
+dem_rock<-dem[filter(clip, crk=="Rock")]
+dem_stoney<-dem[filter(clip, crk=="Stoney")]
+
+plot(dem_stoney) 
+
+#rm(dem)
+
+#is taking a long time but hasn't crashed the computer yet(Update it worked!)
+stoney_poly<-filter(clip, crk=="Stoney")
+
+library(nngeo)
+stoney<-raster_extract(x = dem, y = stoney_poly,na.rm = TRUE)
+stoney<-stoney<=0.75
+
+k<-unlist(stoney)
+l<- k[k <= 0.75]
+
+
+
+saveRDS(l, "stoney_cut.RDS")
+
+
+
+
+
+#The code below makes a true/false raster
+stoney_3<-dem_stoney>=(-3)
+plot(stoney_3)
+
+# DON'T RUN THIS. IT TAKES SO MUCH MEMORY
+# a<-ggplot()+
+#   geom_stars(data=dem_clip)+
+#   theme_stars()
+# a
